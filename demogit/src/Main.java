e5f61f43f2e9bb6fe3a222549639a9cb5c063c1a
fady <108768356+fadymofid@users.noreply.github.com>
2023-12-21 15:49:36 +0200

    initial version

---
+++ demogit/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is prime
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+
+    // Function to check if a number is prime
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        // Check for divisibility from 2 to the square root of the number
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // Not a prime number
+            }
+        }
+
+        return true; // Prime number
+    }
+}
